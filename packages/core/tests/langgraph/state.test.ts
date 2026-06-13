4fd2b3481b650b9dade3080db0a68648b6569ca7
Tom Van Schoor <tom@paymentology.com>
2026-06-13 14:29:38 +0200

    test(st-09065): align state entrypoint imports

--- packages/core/tests/langgraph/state.test.ts
+++ packages/core/tests/langgraph/state.test.ts
@@ -1,4 +1,4 @@
-import './state/create-state-annotation.ts';
-import './state/validate-state.ts';
-import './state/merge-state.ts';
-import './state/defaults-workflow.ts';
+import './state/create-state-annotation.js';
+import './state/validate-state.js';
+import './state/merge-state.js';
+import './state/defaults-workflow.js';
